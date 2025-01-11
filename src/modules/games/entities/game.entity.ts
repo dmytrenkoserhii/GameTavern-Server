d29bf73597444ef0653b0b18184f5fa53ec2aced
Serhii <dmtrnk.srg@gmail.com>
2025-01-11 17:36:13 +0200

    feat(scrum-19): add game entity

---
+++ src/modules/games/entities/game.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+import { List } from '@/modules/lists/entities/list.entity';
+
+@Entity('games')
+@Unique(['gameApiId', 'listId'])
+export class Game {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  gameApiId: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  coverUrl?: string;
+
+  @Column()
+  orderNumber: number;
+
+  @Column()
+  listId: number;
+
+  @ManyToOne(() => List, (list) => list.games)
+  @JoinColumn({ name: 'listId' })
+  list: List;
+}
